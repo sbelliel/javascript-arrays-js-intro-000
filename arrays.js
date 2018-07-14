e8a9de7909130fa283456d8968a65c77db928d9b
Sam Belliel <sbelliel@gmail.com>
2018-07-14 21:40:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,4 +22,6 @@ function destructivelyAddElementToEndOfArray(array, element) {
 
 function accessElementInArray(array, index) {
   console.log(array[index]);
+  var new = array[index];
+  return new;
 }
